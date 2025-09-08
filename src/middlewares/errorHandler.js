3c4b73b044b8f09c1a92142dd310023a2634129b
Anastasia Makarova <anastasiyushka30@gmail.com>
2025-09-08 22:33:45 +0300

    Refactor errorHandler to include error details

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,16 +1,20 @@
-import createError from "http-errors";
+import { HttpError } from "http-errors";
 
 export const errorHandler = (err, req, res, next) => {
-  if (createError.isHttpError(err)) {
+  // Якщо це помилка з createHttpError
+  if (err instanceof HttpError) {
     res.status(err.status).json({
       status: err.status,
-      message: err.message,
+      message: err.name, 
+      data: err,       
     });
     return;
   }
 
+  // Якщо це звичайна помилка (не з createHttpError)
   res.status(500).json({
     status: 500,
     message: "Something went wrong",
+    data: err.message,
   });
 };
