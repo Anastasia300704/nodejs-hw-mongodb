57f4a956a14e86ab727e4d63ba76a864ea5dc79d
Anastasia Makarova <anastasiyushka30@gmail.com>
2025-09-14 20:55:54 +0300

    hw5: готово для проверки

--- src/models/contact.js
+++ src/models/contact.js
@@ -15,6 +15,7 @@ const contactSchema = new mongoose.Schema(
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
+      required: true,
     },
   },
   { timestamps: true, versionKey: false }
