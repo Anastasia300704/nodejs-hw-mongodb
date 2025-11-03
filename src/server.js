6b1600ba65f33e2d47274f76c46ea81439c661f3
Anastasia Makarova <anastasiyushka30@gmail.com>
2025-11-03 17:03:26 +0300

    render

--- src/server.js
+++ src/server.js
@@ -10,8 +10,8 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 
 import notesRoutes from './routes/notesRoutes.js';
-import authRouter from './routes/authRouter.js';
-import userRouter from './routes/userRouter.js';
+import authRoutes from './routes/authRoutes.js';
+import userRouter from './routes/contactsRouter.js';
 
 const app = express();
 
