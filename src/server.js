4e4294b0d1269a6413c045118a2e5bbb42156dc2
Anastasia Makarova <anastasiyushka30@gmail.com>
2025-11-03 16:40:33 +0300

    render

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 
-import notesRouter from './routes/notesRouter.js';
+import notesRoutes from './routes/notesRoutes.js';
 import authRouter from './routes/authRouter.js';
 import userRouter from './routes/userRouter.js';
 
